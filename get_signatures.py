11854cc31ff9bf2e3c559f4790817f1d81070179
naveenr414 <nraman1@DESKTOP-NK1NFCF.localdomain>
2020-04-05 14:31:24 -0400

    Split it up into plot.py and get_signatures.py

--- get_signatures.py
+++ get_signatures.py
@@ -2,7 +2,6 @@ import os
 import glob
 import pandas as pd
 import numpy as np
-from scipy.spatial.distance import cosine 
 
 def create_data():    
     os.system("cd ../tcsm/experiments/simulated-data && snakemake -R simulate_dmr_data --cores 1")
@@ -46,42 +45,6 @@ def factor_tcsm_with_HR_covariate(data_location):
     return pd.read_csv("../tcsm/signatures.tsv",sep="\t")
 
 
-all_locations = create_data()
-methods = {'SomaticSignatures':factor_somatic,'TCSM':factor_tcsm,'TCSM+Covariate':factor_tcsm_with_HR_covariate}
-true_signatures = get_signatures_3_5()
-
-signature_number = 3
-
-for i in methods:
-    f = methods[i]
-    x_loc = []
-    y_loc = []
-
-    for loc in all_locations:
-        factored_signatures = f(loc)
-        num_samples = int(loc.split("/")[-1].split("_")[0])
-
-        pair_similarities = []
-
-        for i in range(len(factored_signatures)):
-            for j in range(len(factored_signatures)):
-                if i!=j:
-                    signature_3_similarity = 1-cosine(factored_signatures[i],true_signatures[0])
-                    signature_5_similarity = 1-cosine(factored_signatures[j],true_signatures[1])
-
-                    pair_similarities[(i,j)] = signature_3_similarity+signature_5_similarity
-                    
-        i,j = max(pair_similarities)
-        x_loc.append(num_samples)
-        if(signature_number == 3):
-            y_loc.append(1-cosine(factored_signatures[i],true_signatures[0]))
-        else:
-            y_loc.append(1-cosine(factored_signatures[j],true_signatures[1]))
-
-    plt.plot(x,y,label=i)
-
-plt.legend()
-plt.savefig("signature_{}_similarity.jpg".format(signature_number))
 
 
         
--- plot.py
+++ plot.py
@@ -1,1 +1,42 @@
- 
+from get_signatures import *
+from scipy.spatial.distance import cosine 
+import matplotlib.pyplot as plt
+
+def plot_signatures(signature_number):
+    all_locations = create_data()
+    methods = {'SomaticSignatures':factor_somatic,'TCSM':factor_tcsm,'TCSM+Covariate':factor_tcsm_with_HR_covariate}
+    true_signatures = get_signatures_3_5()
+
+    for i in methods:
+        f = methods[i]
+        x_loc = []
+        y_loc = []
+
+        for loc in all_locations:
+            factored_signatures = f(loc)
+            num_samples = int(loc.split("/")[-1].split("_")[0])
+
+            pair_similarities = []
+
+            for i in range(len(factored_signatures)):
+                for j in range(len(factored_signatures)):
+                    if i!=j:
+                        signature_3_similarity = 1-cosine(factored_signatures[i],true_signatures[0])
+                        signature_5_similarity = 1-cosine(factored_signatures[j],true_signatures[1])
+
+                        pair_similarities[(i,j)] = signature_3_similarity+signature_5_similarity
+                        
+            i,j = max(pair_similarities)
+            x_loc.append(num_samples)
+            if(signature_number == 3):
+                y_loc.append(1-cosine(factored_signatures[i],true_signatures[0]))
+            else:
+                y_loc.append(1-cosine(factored_signatures[j],true_signatures[1]))
+
+        plt.plot(x,y,label=i)
+
+    plt.legend()
+    plt.savefig("signature_{}_similarity.jpg".format(signature_number))
+
+plot_signatures(3)
+
